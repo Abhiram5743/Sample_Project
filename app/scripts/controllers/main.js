420f7e747765d308e5c5b94ad407ccacccb782b3
btippabhotla <bharadwajt10@gmail.com>
2015-05-13 11:30:18 -0700

    Initial commit to form the project structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nuvisoInitialApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nuvisoInitialApp
+ */
+angular.module('nuvisoInitialApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
